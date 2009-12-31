cb0b0f13946d15de2a9c9dbec43dfee7d9b92c90
Joel Oliveira <joel@carl.local>
2009-12-30 22:51:55 -0500

    queueing the google hosted version of jQuery 1.3.2

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@ if(function_exists('register_sidebar'))
 {
 	//	register_sidebar();
 }
+
+if( !is_admin()){
+   wp_deregister_script('jquery'); 
+   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"), false, '1.3.2'); 
+   wp_enqueue_script('jquery');
+}
 ?>
