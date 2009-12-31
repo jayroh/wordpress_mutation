8a72bae4f59ffb5878b6792bd5ccf3a3f131b415
Joel Oliveira <joel@carl.local>
2009-12-30 22:48:07 -0500

    initial addition and commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+		<div id="footer">
+			<p><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</p>
+			<?php wp_footer(); ?>			
+		</div>
+	</body>
+</html>
