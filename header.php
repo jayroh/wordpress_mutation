e07e5e269462522ae789da67b0f42b8eac2fd37f
jayroh <joel.oliveira@gmail.com>
2010-02-25 12:09:54 -0500

    added some barren default js, including jQuery 1.4.2, put it in the header, and the  syntax highlighting lib

--- header.php
+++ header.php
@@ -6,7 +6,11 @@
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/js/prettify.css" type="text/css" media="screen" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/js/prettify.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 		<?php wp_head(); ?>
 	</head>
